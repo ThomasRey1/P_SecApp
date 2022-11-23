353d0d0fcbf3b90d54891bd6e14c388de59f6a58
Alexandre Samuel King <pd50uyl@eduvaud.ch>
2022-11-23 09:51:38 +0100

    ajout du formulaire

--- captcha.php
+++ captcha.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Captcha</title>
 </head>
+
 <body>
-    <form action="results.php" method="post"></form>
+    <form id="form" class="topBefore" action="result.php" method="POST">
+
+        <input id="name" type="text" placeholder="NAME">
+        <input id="email" type="text" placeholder="E-MAIL">
+        <div class="captcha">
+            Captcha here
+        </div>
+        <input id="captcha" class="captchaAnswer" type="text" placeholder="YOUR ANSWER">
+
+        <input id="submit" type="submit" value="GO!">
+    </form>
 </body>
+
 </html>
